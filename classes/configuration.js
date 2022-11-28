5f337c6f717df13343fe78b64de1a91b431be058
George Wilkins <george@xgeox.com>
2022-11-28 00:31:53 +0000

    Debugging CORS

--- classes/configuration.js
+++ classes/configuration.js
@@ -1,7 +1,7 @@
 class Configuration
 {
     static siteUrl = 'https://georgewilkins.github.io/project-blog/';
-    static wikiUrl = 'https://crossorigin.me/https://github.com/GeorgeWilkins/project-blog/wiki/_pages';
+    static wikiUrl = 'https://github.com/GeorgeWilkins/project-blog/wiki/_pages';
 }
 
 export {Configuration};
