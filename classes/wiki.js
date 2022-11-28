fa05970a1a6292e84ed4d95d2aa254dd45181818
George Wilkins <george@xgeox.com>
2022-11-28 00:28:28 +0000

    Debugging CORS

--- classes/wiki.js
+++ classes/wiki.js
@@ -7,7 +7,7 @@ class Wiki
         const domParser = new DOMParser();
         const indexHtml = await fetch(wikiUrl, {
             method: 'GET',
-            mode: 'no-cors'
+//            mode: 'no-cors'
           })
             .then(response => response.text())
             .catch(error => {
